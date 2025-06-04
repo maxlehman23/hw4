891871eba075a238ac5cf8292c1171eac7e531db
Max Lehman <max.lehman@kellogg.northwestern.edu>
2025-06-04 16:31:54 +0000

    got login name to save at the top

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :current_user
+  helper_method :current_user  # make it available in views
 
   def current_user
-     @current_user = User.find_by({ "id" => session["user_id"] })
+    # memoize so we don't query multiple times per request
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 end
